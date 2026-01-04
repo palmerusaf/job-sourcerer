102b27cb85de72fd48b2f98b32f0cf55d7aefc4b
Branden Palmer <brandenpalmer08@gmail.com>
2026-01-04 12:16:25 -0600

    sort by resume/job match

--- utils/db/getSavedJobs.ts
+++ utils/db/getSavedJobs.ts
@@ -1,11 +1,25 @@
 import { db } from './db';
-import { JobSelectType, jobTable } from './schema';
+import { calculateCosineSimilarity } from '@/utils/extractKeywords';
+import { jobTable, rawResumes } from './schema';
 import { eq } from 'drizzle-orm';
 
-export async function getSavedJobs(): Promise<JobSelectType[]> {
-  const jobs = await db
+export async function getSavedJobs() {
+  const res = await db
     .select()
     .from(jobTable)
-    .where(eq(jobTable.archived, false));
-  return jobs;
+    .where(eq(jobTable.archived, false))
+    .leftJoin(rawResumes, eq(jobTable.resumeId, rawResumes.id));
+  return res
+    .map((i) => {
+      return {
+        ...i.jobs,
+        score: !i?.raw_resumes
+          ? -1
+          : calculateCosineSimilarity(
+            i.jobs.description,
+            i.raw_resumes.rawText
+          ),
+      };
+    })
+    .sort((a, b) => b.score - a.score);
 }
